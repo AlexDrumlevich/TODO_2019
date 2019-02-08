c44ab457fc175f3902d6d425985cb5bc7703ca86
Друмлевич <drumlevic@macbook-pro-drumlevic.local>
2019-02-08 09:36:52 +0500

    Initial Commit

---
+++ TODO_2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODO_2019
+//
+//  Created by Друмлевич on 08.02.2019.
+//  Copyright © 2019 Alexey Drumlevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
